eff37f1afb2a920ee2b035e44c826b6f6ed5beb0
Timba <thiagodesouza07t@gmail.com>
2022-05-05 12:23:20 -0300

    Initial DEV Commit

---
+++ data/db_manager.py
@@ -0,0 +1,16 @@
+def create_db():
+    "Criar DB do alberto"
+    # TO_DO
+    pass
+
+
+def input_templates(**kwargs):
+    "Colocar templates"
+    # TO_DO
+    pass
+
+
+def insert_new_row(**kwargs):
+    "Colocar nova informaçao que o AIberto aprender"
+    # TO_DO
+    pass
